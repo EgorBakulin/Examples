ed8bc31bf961779ef2808acc262fc11d599a1c61
egorbakulin <sham.q54@gmail.com>
2020-12-16 03:04:26 +0700

    tabulation fix

--- parts/form/form.php
+++ parts/form/form.php
@@ -1,8 +1,8 @@
 <form action="index.php">
   <select class="part_selector" name="part">
-    <?php foreach ($dir_names as $option): ?>
-  <option value="<?php echo $option; ?>"><?php echo $option; ?></option>
-    <?php endforeach; ?>
+  <?php foreach ($dir_names as $option): ?>
+      <option value="<?php echo $option; ?>"><?php echo $option; ?></option>
+  <?php endforeach; ?>
   </select>
   <button>select</button>
 </form>
