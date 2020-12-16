ac26257eee76517af53132508be2b190e4c6a900
egorbakulin <sham.q54@gmail.com>
2020-12-16 14:16:47 +0700

    write simle css

--- parts/messages/viev.php
+++ parts/messages/viev.php
@@ -1,17 +1,17 @@
 <div class="messages_container">
-  <div class="message_to">
-    <img class="avatar" src="<?php echo $person1_image;?>" alt="">
-    <div>
-      <div></div>
-      <div></div>
+  <div class="message message_to">
+    <img class="child avatar" src="<?php echo $person1_image;?>" alt="">
+    <div class="child message_box">
+      <div>message</div>
+      <div>message autor</div>
     </div>
   </div>
-  <div class="message_from">
-    <img class="avatar" src="<?php echo $person2_image;?>" alt="">
-    <div>
-      <div></div>
-      <div></div>
+  <div class="message message_from">
+    <div class="child message_box">
+      <div>message</div>
+      <div>message autor</div>
     </div>
+    <img class="child avatar" src="<?php echo $person2_image;?>" alt="">
   </div>
   </div>
 </div>
